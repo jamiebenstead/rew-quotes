8c5fdc43d9710e786be19e3f6ce09b988aeae5a1
jamiebenstead <jamie.benstead@gmail.com>
2025-05-04 20:46:58 +0100

    Randomise quotes, update styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,22 @@ function App() {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const getNextQuote = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % quotes.length);
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setCurrentIndex(randomIndex);
   };
 
   return (
     <div className="outer-container">
       <header className="title-container">
-        <h1 className="title">
-          Andrew, CEO of GoGo Empires
-          <br />
-          <span className="subtitle">Quote Generator</span>
-        </h1>
+        <h1 className="title">Andrew, CEO of GoGo Empires</h1>
+        <h2 className="subtitle">Quote Generator</h2>
       </header>
 
       <main className="app-container">
         <div className="quote-card">
           <h2 className="quote">{quotes[currentIndex]}</h2>
           <button className="refresh-button" onClick={getNextQuote}>
-            Refresh
+            Next Quote
           </button>
         </div>
       </main>
